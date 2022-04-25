1d2e1a31c4c9d5eef9c3996334eda8513f518a19
Amir Souri <ahsouri@cfa.harvard.edu>
2022-04-25 16:44:39 -0400

    add saver and subsetter

--- demalos/demalos.py
+++ demalos/demalos.py
@@ -33,7 +33,7 @@ class demalos(object):
                   self.alos_folder_in = alos_folder_in
                   self.alos_folder_out = alos_folder_out
             else:
-                raise TypeError("only folders are allowed!")
+                raise TypeError("paths are either not a folder or non-existent")
 
     def download_alos(self,dem_file = None):
         
@@ -43,7 +43,6 @@ class demalos(object):
            currently thhe package doesn't download a subset 
            based on lat/lon, unless you specify exactly the 
            name of DEM file
-
         '''
         jaxa_url = 'https://www.eorc.jaxa.jp/ALOS/aw3d30/data/release_v2012/'
 
@@ -85,3 +84,115 @@ class demalos(object):
         # Don't use print() as it will print in new line every time.
         sys.stdout.write("\r" + progress_message)
         sys.stdout.flush()
+
+    def subset_alos(self):
+        
+        '''
+           subset/merge alos geotiff files based on lat0/lon0
+        '''
+
+        import rasterio
+        import glob
+        from rasterio.merge import merge
+        from shapely.geometry import Polygon
+        import numpy as np
+        import matplotlib.pyplot as plt
+        
+        within_box = []
+        intersect_box = []
+        # sort tiff files
+        alos_fname = sorted(glob.glob(self.alos_folder_in + '/*DSM.tif'))
+
+        for fname in alos_fname:
+            try:
+                src = rasterio.open(fname)
+            except:
+                continue
+        
+            out_trans = src.transform
+            # check the boundaries and make polygons
+            width = src.width
+            height = src.height
+
+            corner1 =  out_trans * (0,0)     
+            corner4 =  out_trans * (height,width)
+
+            pol_alos = Polygon([(corner1[0],corner4[1]), (corner4[0],corner4[1]), (corner4[0],corner1[1]), 
+                             (corner1[0],corner1[1]), (corner1[0],corner4[1])])
+
+            pol_target = Polygon([(self.lon0,self.lat0), 
+                          (self.lon0+self.dlon,self.lat0),
+                          (self.lon0+self.dlon,self.lat0+self.dlat), 
+                          (self.lon0,self.lat0+self.dlat),
+                          (self.lon0,self.lat0)])
+
+            #x,y = pol_alos.exterior.xy
+            #plt.plot(x, y, c = "blue")
+            #print(x,y)
+            #x,y = pol_target.exterior.xy
+            #print(x,y)
+            #plt.plot(x, y, c = "red")
+            #plt.show()
+           
+            if (pol_alos.contains(pol_target)):
+               within_box.append(fname)
+            elif (pol_alos.intersects(pol_target)) and (pol_alos.intersection(pol_target).area>0.0):
+               intersect_box.append(fname)
+        
+        if ((not within_box) and (not intersect_box)):
+            raise TypeError("the pool data do not cover this area; download more!")
+            
+        if (not within_box) and (intersect_box):
+            src_appended = []
+            for int_box in range(len(intersect_box)):
+                src = rasterio.open(intersect_box[int_box])
+                src_appended.append(src)
+                
+            alos_dem, out_trans = merge(src_appended)
+            print('Several tiles are chosen from the pool')
+            # if there is at least one master to fully enclose the slave
+        elif within_box:           
+            print('The chosen ALOS file is ' +  within_box[0])
+            src = rasterio.open(within_box[0])
+            out_trans = src.transform
+            alos_dem = src.read(1)
+
+        # getting lat and lons based on the transformation
+        lat_alos = np.zeros_like(alos_dem)*np.nan
+        lon_alos = np.zeros_like(alos_dem)*np.nan
+        for i in range(np.shape(alos_dem)[0]):
+            for j in range(np.shape(alos_dem)[1]):
+                temp = out_trans * (j,i)
+                lat_alos[i,j] = temp[0] 
+                lon_alos[i,j] = temp[1]
+
+        self.lat_alos = np.float32(lat_alos)
+        self.lon_alos = np.float32(lon_alos)
+        self.alos_dem = np.array(alos_dem)
+        self.crs = src.crs
+        self.transform = out_trans
+
+    def save_to_tiff(self,filename):
+         
+        '''
+           save the outputs to a geotiff file (filename) 
+           in alos_folder_out folder
+        '''
+
+        import rasterio
+        import numpy as np  
+            
+        new_tiff = rasterio.open(
+            self.alos_folder_out + str(filename),
+            'w',
+            driver='GTiff',
+            height=self.alos_dem.shape[0],
+            width=self.alos_dem.shape[1],
+            count=1,
+            dtype=np.float32,
+            crs=self.crs,
+            transform=self.transform,
+            )
+
+        new_tiff.write(self.alos_dem,1)
+        new_tiff.close()
--- demalos/example.py
+++ demalos/example.py
@@ -1,6 +1,7 @@
 from demalos import demalos
 
-alos_obj = demalos(50,50,10,10,alos_folder_in='/Volumes/Amir_5TB/SAO/ALOS_DEM/in',
-                   alos_folder_out='/Volumes/Amir_5TB/SAO/ALOS_DEM/out')
+alos_obj = demalos(41,-112,0.2,0.2,alos_folder_in='/Volumes/Amir_5TB/SAO/ALOS_DEM/in/N040W115_N045W110',
+                   alos_folder_out='/Volumes/Amir_5TB/SAO/ALOS_DEM/out/')
 
-alos_obj.download_alos(dem_file='N040W115_N045W110')
+alos_obj.subset_alos()
+alos_obj.save_to_tiff("output_test.tif")
